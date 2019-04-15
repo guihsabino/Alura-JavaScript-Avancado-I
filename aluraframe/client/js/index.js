e8726762facfe892b3fb3548780404c1e2f0aa79
guihsabino <guilhermesabino0210@outlook.com>
2019-04-15 16:53:07 -0300

    commit

--- aluraframe/client/js/index.js
+++ aluraframe/client/js/index.js
@@ -8,6 +8,13 @@ var campos = [
 console.log(campos);
 // Adicionando uma função ao Botão Incluir para dar Submit
 document.querySelector(".form").addEventListener('submit', function (event) {
+    // Criando uma tr a cada vez que o botão for acionado
+    var tr = document.createElement("tr");
 
-    alert("oi");
+    campos.forEach(function (campo) {
+
+        var td = document.createElement("td");
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
 });
