042f58682141ac31da05342f85b37ccdb27eb04a
guihsabino <guilhermesabino0210@outlook.com>
2019-04-16 12:45:12 -0300

    commit

---
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,13 @@
+class NegociacaoController {
+
+    adiciona(event) {
+        event.preventDefault();
+
+        let inputData = document.querySelector('#data');
+        let inputQuantidade = document.querySelector('#quantidade');
+        let inputValor = document.querySelector('#valor');
+
+        
+    }
+
+}
