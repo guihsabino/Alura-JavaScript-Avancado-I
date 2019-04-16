b958d101c2ea2f8b51de478796151a675d6c7f0a
guihsabino <guilhermesabino0210@outlook.com>
2019-04-16 10:31:12 -0300

    commi

--- aluraframe/client/js/index.js
+++ aluraframe/client/js/index.js
@@ -6,17 +6,36 @@ var campos = [
 ];
 
 console.log(campos);
+// Buscando o tbody do HTML
+var tbody = document.querySelector('table tbody');
+
 // Adicionando uma função ao Botão Incluir para dar Submit
 document.querySelector(".form").addEventListener('submit', function (event) {
+
+    event.preventDefault();
     // Criando uma tr a cada vez que o botão for acionado
     var tr = document.createElement("tr");
 
     campos.forEach(function (campo) {
-
+        // Colocando as tds dentro das trs
         var td = document.createElement("td");
         td.textContent = campo.value;
         tr.appendChild(td);
     });
+    // Criando a td volume
+    var tdVolume = document.createElement('td');
+    // Definindo que o conteudo de volume é a multiplicação de valor e qntd
+    tdVolume.textContent = campo[1].value * campo[2].value;
 
-    var tdVolume
+    tr.appendChild(tdVolume);
+
+    tbody.appendChild(tr);
+    // Limpando os campos e deixando os valores padrões de inicio
+    campos[0].value = '';
+    campos[1].value = 1;
+    campos[2].value = 0;
+
+    campos[0].focus();
+
+
 });
