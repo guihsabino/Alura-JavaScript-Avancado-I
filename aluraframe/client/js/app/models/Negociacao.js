db9ef7bba1d55513c9bfc71f368c14d0c19b390b
guihsabino <guilhermesabino0210@outlook.com>
2019-04-16 11:18:44 -0300

    commit

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -1,6 +1,7 @@
 class Negociacao {
     // Esse construtor já seta o que cada negociação vai ter por padrão
     constructor(data, quantidade, valor) {
+
         this.data = data;
         this.quantidade = quantidade;
         this.valor = valor;
