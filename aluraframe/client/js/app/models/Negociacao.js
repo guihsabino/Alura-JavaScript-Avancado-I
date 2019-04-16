d6f3e4707add3aaaa0e9fd74cdc6f377ff1b7c7b
guihsabino <guilhermesabino0210@outlook.com>
2019-04-16 11:30:50 -0300

    commit

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -2,12 +2,27 @@ class Negociacao {
     // Esse construtor já seta o que cada negociação vai ter por padrão
     constructor(data, quantidade, valor) {
 
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
-    // Criando método que obtem o volume da negociação
-    obtemVolume() {
-        return this.quantidade * this.valor;
+
+    /* Metodos acessadores, ja que tem o (_) pra não permitir acesso externo à classe */
+
+    getVolume() {
+        // obtem e calcula o volume da negociação
+        return this._quantidade * this._valor;
     }
+    getData() {
+        return this._data;
+    }
+    getQuantidade() {
+        return this._quantidade;
+    }
+    getValor() {
+        return this._valor;
+    }
+
+
+
 }
