9b34922d8cec8e228ed00b99a205a0e419fc9b89
guihsabino <guilhermesabino0210@outlook.com>
2019-04-16 10:59:59 -0300

    commit

---
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -0,0 +1,8 @@
+class Negociacao {
+    // Esse construtor já seta o que cada negociação vai ter por padrão
+    constructor() {
+        this.data = new Date();
+        this.quantidade = 1;
+        this.valor = 0;
+    }
+}
