ca3645a0bbf7d428d4813fc4b55a80df0bb28998
guihsabino <guilhermesabino0210@outlook.com>
2019-04-16 12:54:54 -0300

    commit

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -3,8 +3,8 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
 
-        // Atribuindo a um caracter a função de selecionar, pra facilitar
-        let $ = documento.querySelector;
+        // Atribuindo a um caracter a função de selecionar, pra facilitar (bind serve pra manter o document na jogada)
+        let $ = document.querySelector.bind(document);
 
         let inputData = $('#data');
         let inputQuantidade = $('#quantidade');
